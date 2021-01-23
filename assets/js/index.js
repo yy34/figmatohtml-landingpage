1581fcf32a18e192c305d06da016c86cfa129a1c
Yunus YILMAZ <yy3474@yandex.com>
2021-01-23 21:31:56 +0300

    update

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
+
+sr.reveal(".hero-image", {});
+sr.reveal(".video-section", { delay: 200 });
+sr.reveal(".features-section", { delay: 400 });
+sr.reveal(".tech-section", { interval: 200 });
+sr.reveal(".appdownload", { interval: 200 });
+sr.reveal(".contact", { interval: 200 });
+sr.reveal(".footer", { interval: 200 });
